3a99cdfe89c38e80a96414b5ec45f316ecff386a
Spud <jeffrypig23@gmail.com>
2019-10-12 12:13:51 -0800

    Got it to track a bus!

--- app/src/main/java/fnsb/macstransit/MapsActivity.java
+++ app/src/main/java/fnsb/macstransit/MapsActivity.java
@@ -3,23 +3,64 @@ package fnsb.macstransit;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-	private GoogleMap mMap;
+	/**
+	 * A boolean to check where or not to run the update thread
+	 */
+	public boolean run = false;
 
-	// Create the string list of routes
-	private String[] routes = new String[]{"Red", "Blue", "Yellow", "Gold", "Brown"};
+	/**
+	 * Create an array to store all the markers for the buses
+	 */
+	ArrayList<Marker> markers = new ArrayList<>();
 
-	// Create an instance of routematch
+	/**
+	 * Create our map object
+	 */
+	private GoogleMap map;
+
+	/**
+	 * Create a string array of all the routes
+	 */
+	private String[] routes = new String[]{"Blue", "Brown", "Gold", "Green", "Purple", "Red", "Yellow"};
+
+	/**
+	 * Create an instance of the route match object
+	 */
 	private RouteMatch routeMatch = new RouteMatch("fnsb", "https://fnsb.routematch.com/feed/vehicle/byRoutes/", this.routes);
+
+	public static double[] getLatLong(JSONObject jsonObject) {
+		try {
+			//System.out.println(jsonObject.toString());
+			Log.d("getLatLong", "Full object: " + jsonObject.toString());
+			String lat = jsonObject.getString("latitude");
+			Log.d("Latitude", lat);
+			String lng = jsonObject.getString("longitude");
+			Log.d("Longitude", lng);
+
+			return new double[]{Double.parseDouble(lat), Double.parseDouble(lng)};
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +79,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 		// TODO
 		// Here is where I want to start (or restart as the case may be) the loop to check for position updates
 		// This is mostly for data saving measures
+		this.run = true;
+
+		this.thread().start();
+
+
 	}
 
 	@Override
@@ -45,8 +91,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 		super.onPause();
 		// TODO
 		// Here is where I want to stop the update cycle that queries the routematch server
+		this.run = false;
 	}
 
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// TODO
+		// Here is where I want to stop the update cycle that queries the routematch server
+		this.run = false;
+	}
 
 	/**
 	 * Manipulates the map once available.
@@ -59,11 +113,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 	 */
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
-		mMap = googleMap;
+		map = googleMap;
 
 		// Add a marker in Sydney and move the camera
 		LatLng home = new LatLng(64.8391975, -147.7684709);
-		//mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 11.0f));
+		//map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 11.0f));
+	}
+
+
+	/*
+	Thread stuff beyond here. You have been warned
+	 */
+	private Thread thread() {
+		Thread t = new Thread(() -> {
+
+			Log.w("Update thread", "Starting up...");
+
+			while (this.run && !Thread.interrupted()) {
+
+				for (Marker marker : this.markers) {
+					runOnUiThread(marker::remove);
+				}
+
+				try {
+					JSONArray array = this.routeMatch.getRoute("Blue").getJSONArray("data");
+
+					//Log.i("object", object.toString());
+					for (int i = 0; i < array.length(); i++) {
+						JSONObject object = array.getJSONObject(i);
+						final double[] latlong = getLatLong(object);
+						if (latlong != null) {
+							runOnUiThread(() -> {
+								Marker marker = this.map.addMarker(new MarkerOptions().position(new LatLng(latlong[0], latlong[1])).title("Blue"));
+								marker.setVisible(true);
+								this.markers.add(marker);
+							});
+						}
+
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				Thread.yield();
+				Log.d("Update thread", "Looping...");
+
+			}
+
+			Log.w("Update thread", "Shutting down...");
+		});
+
+		return t;
+
 	}
 }
