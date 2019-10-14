72721f19e1062b51fc932395fc3ff27076fe5c2b
Spud <jeffrypig23@gmail.com>
2019-10-13 17:46:00 -0800

    Added documentation to the MapsActivity (the main activity)
    Issue #6 can finally be closed :tada:

--- app/src/main/java/fnsb/macstransit/MapsActivity.java
+++ app/src/main/java/fnsb/macstransit/MapsActivity.java
@@ -5,7 +5,6 @@ import android.view.Menu;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 
 import java.util.ArrayList;
 
@@ -36,7 +35,7 @@ public class MapsActivity extends androidx.fragment.app.FragmentActivity impleme
 	public ArrayList<Route> selectedRoutes = new ArrayList<>();
 
 	/**
-	 * TODO Documentation
+	 * Create an array of all the buses that will end up being tracked.
 	 */
 	public ArrayList<Bus> buses = new ArrayList<>();
 
@@ -46,13 +45,20 @@ public class MapsActivity extends androidx.fragment.app.FragmentActivity impleme
 	private GoogleMap map;
 
 	/**
-	 * TODO Documentation
+	 * Create an instance of the thread object that will be used to pull data from the routematch server.
 	 */
 	private UpdateThread thread = new UpdateThread(this, 4000);
 
 	/**
-	 * Gets called every time the user presses the menu button.
-	 * Use if your menu is dynamic.
+	 * Prepare the Screen's standard options menu to be displayed.
+	 * This is called right before the menu is shown, every time it is shown.
+	 * You can use this method to efficiently enable/disable items or otherwise dynamically modify the contents.
+	 * <p>
+	 * The default implementation updates the system menu items based on the activity's state.
+	 * Deriving classes should always call through to the base class implementation.
+	 *
+	 * @param menu The options menu as last shown or first initialized by onCreateOptionsMenu().
+	 * @return You must return true for the menu to be displayed; if you return false it will not be shown.
 	 */
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
@@ -70,10 +76,14 @@ public class MapsActivity extends androidx.fragment.app.FragmentActivity impleme
 	}
 
 	/**
-	 * TODO Documentation
+	 * This hook is called whenever an item in your options menu is selected.
+	 * The default implementation simply returns false to have the normal processing happen (calling the item's Runnable or sending a message to its Handler as appropriate).
+	 * You can use this method for any items for which you would like to do processing without those other facilities.
+	 * <p>
+	 * Derived classes should call through to the base class for it to perform the default menu handling.
 	 *
-	 * @param item
-	 * @return
+	 * @param item The menu item that was selected. This value must never be null.
+	 * @return Return false to allow normal menu processing to proceed, true to consume it here.
 	 */
 	@Override
 	public boolean onOptionsItemSelected(android.view.MenuItem item) {
@@ -122,22 +132,39 @@ public class MapsActivity extends androidx.fragment.app.FragmentActivity impleme
 	}
 
 	/**
-	 * TODO Documentation
+	 * Called when the activity loses foreground state,
+	 * is no longer focusable or before transition to stopped/hidden or destroyed state.
+	 * The activity is still visible to user, so it's recommended to keep it visually active and continue updating the UI.
+	 * Implementations of this method must be very quick because the next activity will not be resumed until this method returns.
+	 * Followed by either onResume() if the activity returns back to the front, or onStop() if it becomes invisible to the user.
+	 * <p>
+	 * Here is where I want to stop the update cycle that queries the routematch server.
 	 */
 	@Override
 	protected void onPause() {
 		super.onPause();
-		// Here is where I want to stop the update cycle that queries the routematch server
 		this.thread.run = false;
 	}
 
 	/**
-	 * TODO Documentation
+	 * Perform any final cleanup before an activity is destroyed.
+	 * This can happen either because the activity is finishing (someone called finish() on it),
+	 * or because the system is temporarily destroying this instance of the activity to save space.
+	 * You can distinguish between these two scenarios with the isFinishing() method.
+	 * <p>
+	 * Note: <i>do not count on this method being called as a place for saving data! For example,
+	 * if an activity is editing data in a content provider,
+	 * those edits should be committed in either onPause() or onSaveInstanceState(Bundle), not here.
+	 * This method is usually implemented to free resources like threads that are associated with an activity,
+	 * so that a destroyed activity does not leave such things around while the rest of its application is still running.
+	 * There are situations where the system will simply kill the activity's hosting process without calling this method (or any others) in it,
+	 * so it should not be used to do things that are intended to remain around after the process goes away.</i>
+	 * <p>
+	 * Here is where I want to stop the update cycle that queries the routematch server.
 	 */
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		// Here is where I want to stop the update cycle that queries the routematch server
 		this.thread.run = false;
 	}
 
@@ -159,48 +186,53 @@ public class MapsActivity extends androidx.fragment.app.FragmentActivity impleme
 	}
 
 	/**
-	 * TODO Documentation and add better comments
-	 * <p>
-	 * TODO Also, clean this shit up
+	 * Updates the position (and color) of the markers on the map.
 	 */
 	public void updateBusMarkers() {
+
+		// Start by iterating through all the buses that are currently being tracked.
 		for (Bus bus : this.buses) {
+
+			// The following should be done on the UI thread
 			this.runOnUiThread(() -> {
 
-				// Get the old marker
-				Marker marker = bus.getMarker();
+				// Get the old marker for the bus
+				com.google.android.gms.maps.model.Marker marker = bus.getMarker();
 
 				// Get the current LatLng of the bus
 				LatLng latLng = new LatLng(bus.latitude, bus.longitude);
 
-				if (marker != null) {
-					// Update the markers position to the new LatLong
-					marker.setPosition(latLng);
-					marker.setTitle(bus.route.routeName);
-					if (bus.route.color != 0) {
-						marker.setIcon(this.getMarkerIcon(bus.route.color));
-					}
-					marker.setVisible(true);
-					bus.setMarker(marker);
-				} else {
-					Marker newMarker = this.map.addMarker(new com.google.android.gms.maps.model.MarkerOptions());
-					newMarker.setPosition(latLng);
-					newMarker.setTitle(bus.route.routeName);
-					if (bus.route.color != 0) {
-						newMarker.setIcon(this.getMarkerIcon(bus.route.color));
-					}
-					newMarker.setVisible(true);
-					bus.setMarker(newMarker);
+				// Check if that bus has a marker to begin with.
+				// If the bus doesn't have a marker create a new one,
+				// and overwrite the marker variable with the newly created marker
+				if (marker == null) {
+					marker = this.map.addMarker(new com.google.android.gms.maps.model.MarkerOptions());
 				}
+
+				// Now update the markers position and title
+				marker.setPosition(latLng);
+				marker.setTitle(bus.route.routeName);
+
+				// If the route has a color, set its icon to that color
+				if (bus.route.color != 0) {
+					marker.setIcon(this.getMarkerIcon(bus.route.color));
+				}
+
+				// Make sure that the marker is visible
+				marker.setVisible(true);
+
+				// Finally, (re)assign the marker to the bus
+				bus.setMarker(marker);
 			});
 		}
 	}
 
 	/**
-	 * TODO Documentation
+	 * Gets the color of the marker icon based off of the color value given.
+	 * The reason why there needs to be a function for this is because there are only 10 colors that a marker icon can be.
 	 *
-	 * @param color
-	 * @return
+	 * @param color The desired color value as an int.
+	 * @return The BitmapDescriptor used for defining the color of a markers's icon.
 	 */
 	private com.google.android.gms.maps.model.BitmapDescriptor getMarkerIcon(int color) {
 		float[] hsv = new float[3];
