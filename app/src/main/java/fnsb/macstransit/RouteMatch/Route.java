62d5856516022dda894c664002436a6c49843a2c
Spud <jeffrypig23@gmail.com>
2019-10-17 11:19:52 -0800

    Cleaned up Route.java, and added documentaiton

--- app/src/main/java/fnsb/macstransit/RouteMatch/Route.java
+++ app/src/main/java/fnsb/macstransit/RouteMatch/Route.java
@@ -1,16 +1,11 @@
 package fnsb.macstransit.RouteMatch;
 
-import android.graphics.Color;
 import android.util.Log;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
-
-import fnsb.macstransit.Exceptions.RouteMatchException;
 
 /**
  * Created by Spud on 2019-10-12 for the project: MACS Transit.
@@ -45,7 +40,7 @@ public class Route {
 	 *
 	 * @param routeName The name of the route.
 	 * @param color     The routes color. This is optional,
-	 *                  and of the color is non-existent simply use the {@code Route(String routeName)} constrctor.
+	 *                  and of the color is non-existent simply use the {@code Route(String routeName)} constructor.
 	 */
 	public Route(String routeName, int color) {
 		this(routeName);
@@ -53,36 +48,41 @@ public class Route {
 	}
 
 	/**
-	 * TODO Documentaiton and comments
+	 * Dynamically generates the routes that are used by parsing the master schedule.
 	 *
-	 * @return
+	 * @return An array of routes that <b><i>can be</i></b> tracked.
 	 */
 	public static Route[] generateRoutes(String url) throws InterruptedException {
+
+		// Create an array to store all the generated routes. This will be returned in the end.
 		ArrayList<Route> routes = new ArrayList<>();
 
-		// TODO Fix exception handling
+		// Run the following on a new thread (as android doesn't like running network requests on the UI thread).
 		Thread t = new Thread(() -> {
-			// First, get the master schedual from the provided url
-			JSONObject masterSchedual = RouteMatch.readJsonFromUrl(url + "masterRoute");
-			Log.d("Master Schedual", masterSchedual.toString());
+			// First, get the master schedule from the provided url
+			JSONObject masterSchedule = RouteMatch.readJsonFromUrl(url + "masterRoute");
+			Log.d("Master Schedule", masterSchedule.toString());
 
-			// Now get the data array from the JSON oblect
-			JSONArray data = null;
+			// Now get the data array from the JSON object
+			org.json.JSONArray data;
 			try {
-				data = masterSchedual.getJSONArray("data");
+				data = masterSchedule.getJSONArray("data");
 			} catch (JSONException e) {
 				e.printStackTrace();
+				// If there was a JSONException in parsing the data, just return from the thread now!
 				return;
 			}
 
-			Log.d("Schedual data", data.toString());
+			// Display the schedule data for debugging purposes.
+			Log.d("Schedule data", data.toString());
 
-			// Itterate through the data array to begin parsing the routes
+			// Iterate through the data array to begin parsing the routes
 			for (int index = 0; index < data.length(); index++) {
-				JSONObject routeData = null;
+				JSONObject routeData;
 				try {
-					routeData = data.getJSONObject(index);
 
+					// Get the routeData that we are currently parsing as its own JSONObject variable.
+					routeData = data.getJSONObject(index);
 					Log.d("routeData", "Parsing routeData: " + routeData);
 
 					// First, parse the name
@@ -91,7 +91,7 @@ public class Route {
 
 					// Now try to parse the color
 					try {
-						int color = Color.parseColor(routeData.getString("routeColor"));
+						int color = android.graphics.Color.parseColor(routeData.getString("routeColor"));
 						routes.add(new Route(name, color));
 					} catch (IllegalArgumentException | JSONException colorError) {
 						Log.w("generateRoutes", "Unable to determine route color");
@@ -101,12 +101,15 @@ public class Route {
 
 				} catch (JSONException e) {
 					e.printStackTrace();
+					// If there was a JSONException with either the routeData, or the name,
+					// just break from the loop now.
+					// This will also cause the thread to return as this is the last thing in the thread.
 					break;
 				}
 			}
 		});
 		t.start();
-		t.join(1000);
+		t.join();
 
 		return routes.toArray(new Route[0]);
 	}
