590749ee6819982b44670d3680b725c8d9dfa8c6
Spud <jeffrypig23@gmail.com>
2019-11-28 12:49:58 -0800

    Added documentation for Route

--- app/src/main/java/fnsb/macstransit/RouteMatch/Route.java
+++ app/src/main/java/fnsb/macstransit/RouteMatch/Route.java
@@ -2,9 +2,7 @@ package fnsb.macstransit.RouteMatch;
 
 import android.util.Log;
 
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.Polyline;
 
 import org.json.JSONArray;
@@ -13,10 +11,6 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import fnsb.macstransit.Activities.ActivityListeners.Helpers;
-import fnsb.macstransit.Activities.MapsActivity;
-import fnsb.macstransit.Activities.SettingsPopupWindow;
 
 /**
  * Created by Spud on 2019-10-12 for the project: MACS Transit.
@@ -48,17 +42,18 @@ public class Route {
 	public Stop[] stops;
 
 	/**
-	 * TODO Documentation
+	 * The array of buses that correspond to this route.
 	 */
 	public Bus[] buses;
 
 	/**
-	 * TODO Documentation
+	 * The array of LatLng coordinates that will be used to create the polyline (if enabled).
+	 * This should be initialized with an array of length 0.
 	 */
 	public LatLng[] polyLineCoordinates = new LatLng[0];
 
 	/**
-	 * TODO Documentation
+	 * The polyline that corresponds to this route. This may be null if not enabled.
 	 */
 	private Polyline polyline;
 
@@ -93,10 +88,10 @@ public class Route {
 
 	/**
 	 * Dynamically generates the routes that are used by parsing the master schedule.
-	 * This may return an empty route array if there was an issue parsing the data.
-	 * TODO Update documentation
+	 * This may return an empty route array if there was an issue parsing the data,
+	 * or if there were no routes to parse based off the master schedule.
 	 *
-	 * @param masterSchedule
+	 * @param masterSchedule The master schedule JSONObject from teh RouteMatch server
 	 * @return An array of routes that <b><i>can be</i></b> tracked.
 	 */
 	public static Route[] generateRoutes(JSONObject masterSchedule) {
@@ -118,10 +113,10 @@ public class Route {
 				// Get the routeData that we are currently parsing as its own JSONObject variable.
 				org.json.JSONObject routeData = data.getJSONObject(index);
 
-				// First, parse the name
+				// First, parse the name.
 				String name = routeData.getString("shortName");
 
-				// Now try to parse the color
+				// Now try to parse the color.
 				try {
 					int color = android.graphics.Color.parseColor(routeData.getString("routeColor"));
 					routes.add(new Route(name, color));
@@ -143,19 +138,22 @@ public class Route {
 	}
 
 	/**
-	 * TODO Documentation
+	 * Enabled a route by route name,
+	 * and returns a new array of enabled routes including the previously enabled routes.
 	 *
-	 * @param routeName
-	 * @param oldRoutes
-	 * @return
+	 * @param routeName The new route to enable (by route name).
+	 * @param oldRoutes The array of old routes that were previously enabled.
+	 *                  If there were no previously enabled routes then this must be an array of size 0.
+	 * @return The array of routes that are now being tracked.
 	 */
 	public static Route[] enableRoutes(String routeName, Route[] oldRoutes) {
 		Log.d("enableRoutes", "Enabling route: " + routeName);
 
+		// Make a copy of the oldRoutes array, but have it be one sizer bigger.
 		Route[] routes = Arrays.copyOf(oldRoutes, oldRoutes.length + 1);
 
 		// If the route is to be enabled, iterate through all the allRoutes that are able to be tracked.
-		for (Route route : MapsActivity.allRoutes) {
+		for (Route route : fnsb.macstransit.Activities.MapsActivity.allRoutes) {
 
 			// If the route that is able to be tracked is equal to that of the route entered as an argument,
 			// add that route to the selected allRoutes array.
@@ -172,41 +170,56 @@ public class Route {
 				break;
 			}
 		}
+
+		// Return the newly enabled routes as an array.
 		return routes;
 	}
 
 	/**
-	 * TODO Documentation
+	 * Disables a route by name, and removes all elements of that route from the map (except stops).
 	 *
-	 * @param routeName
-	 * @param oldRoutes
-	 * @return
+	 * @param routeName The route the be disabled by name.
+	 * @param oldRoutes The array of old, though currently enabled routes.
+	 * @return The array of routes still to be tracked,
+	 * with the omission of the route that was to be removed.
+	 * If there are no more routes that are to be enabled, then an array of size 0 will be returned.
 	 */
 	public static Route[] disableRoute(String routeName, Route[] oldRoutes) {
 		Log.d("disableRoute", "Disabling route: " + routeName);
 
+		// Convert all the old routes to an array list of routes.
 		ArrayList<Route> routes = new ArrayList<>(Arrays.asList(oldRoutes));
 
+		// Iterate through the currently enabled routes.
 		for (Route route : routes) {
+
+			// If the route name of the current route matches that of the route to be disabled,
+			// execute the following:
 			if (route.routeName.equals(routeName)) {
+
+				// Remove the buses from the map as well as the route.
 				for (Bus bus : route.buses) {
-					Marker marker = bus.getMarker();
+					com.google.android.gms.maps.model.Marker marker = bus.getMarker();
 					if (marker != null) {
 						bus.getMarker().remove();
 					}
 				}
 				route.buses = new Bus[0];
 
+				// Remove the polyline from the map as well as the route.
 				Polyline polyline = route.getPolyline();
 				if (polyline != null) {
 					polyline.remove();
 					route.polyline = null;
 				}
 
+				// Finally remove the route from the array of enabled routes,
+				// and break since we are only removing one route per call.
 				routes.remove(route);
 				break;
 			}
 		}
+
 
 		return routes.toArray(new Route[0]);
 	}
@@ -270,26 +283,38 @@ public class Route {
 	}
 
 	/**
-	 * TODO Documentation
+	 * Declares and initializes an array of LatLng coordinates to be used by the routes polyline.
 	 *
-	 * @param routeMatch
-	 * @return
+	 * @param routeMatch The RouteMatch object.
+	 * @return The initialized array of LatLng coordinates.
 	 */
 	public LatLng[] loadPolyLineCoordinates(RouteMatch routeMatch) {
-		JSONArray data = RouteMatch.parseData(routeMatch.getLandRoute(this));
+
+		// Get the JSONArray of points for this route from the RouteMatch server.
 		JSONArray points = null;
 		try {
-			points = data.getJSONObject(0).getJSONArray("points");
+			points = RouteMatch.parseData(routeMatch.getLandRoute(this)).getJSONObject(0)
+					.getJSONArray("points");
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
+
+		// If the points are not null execute the following:
 		if (points != null) {
+			// Get the number of points in the array.
 			int count = points.length();
+
+			// Create a new array of LatLng coordinates with the size being that of the number of points in the array.
 			LatLng[] coordinates = new LatLng[count];
+
+			// Initialize the array fo coordinates by iterating through the JSONArray.
 			for (int index = 0; index < count; index++) {
 				Log.d("loadPolyLineCoordinates", String.format("Parsing coordinate %d/%d", index + 1, count));
 				try {
+					// Get the JSONObject at the current index of the array.
 					JSONObject object = points.getJSONObject(index);
+
+					// Set the LatLng object to the latitude and longitude data from the JSONObject.
 					LatLng latLng = new LatLng(object.getDouble("latitude"), object.getDouble("longitude"));
 					coordinates[index] = latLng;
 				} catch (JSONException e) {
@@ -298,25 +323,26 @@ public class Route {
 			}
 			return coordinates;
 		} else {
+			// Since the points were null, just return a LatLng array of size 0.
 			return new LatLng[0];
 		}
 	}
 
 	/**
-	 * TODO Documentation
+	 * Returns the polyline that corresponds to the route.
 	 *
-	 * @return
+	 * @return The polyline that corresponds to the route.
 	 */
 	public Polyline getPolyline() {
 		return this.polyline;
 	}
 
 	/**
-	 * TODO Documentation
+	 * Creates and sets the polyline for the route, as well as adds it to the map.
 	 *
-	 * @param map
+	 * @param map The map to add the polyline to.
 	 */
-	public void createPolyline(GoogleMap map) {
-		this.polyline = Helpers.createPolyLine(this, map);
+	public void createPolyline(com.google.android.gms.maps.GoogleMap map) {
+		this.polyline = fnsb.macstransit.Activities.ActivityListeners.Helpers.createPolyLine(this, map);
 	}
 }
