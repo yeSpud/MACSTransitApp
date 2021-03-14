40c0b202879aae71337284b03bfea575d97b7dc1
Spud <sogden3@alaska.edu>
2021-03-13 20:10:47 -0900

    Cleaned up Network

--- app/src/main/java/fnsb/macstransit/Threads/Network.java
+++ app/src/main/java/fnsb/macstransit/Threads/Network.java
@@ -2,6 +2,8 @@ package fnsb.macstransit.Threads;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -11,12 +13,12 @@ import java.net.SocketTimeoutException;
 /**
  * Created by Spud on 2019-10-21 for the project: MACS Transit.
  * <p>
- * For the license, view the file titled LICENSE at the root of the project
+ * For the license, view the file titled LICENSE at the root of the project.
  * <p>
  * <i><b>Yo dawg. I heard you like static methods. So I got you some static methods,
  * so you could static method while you static method :D</b></i>
- * <p>
- * <p>
+ * <br >
+ * <br >
  * <i>Ok, I've had my fun.</i>
  * <p>
  * But in all seriousness, all of these methods are static, but only one is public,
@@ -27,7 +29,7 @@ import java.net.SocketTimeoutException;
  * this needs to be done on its own thread.
  * Hence the complexity :(
  *
- * @version 1.2
+ * @version 1.2.
  * @since Beta 6.
  */
 public class Network {
@@ -47,17 +49,20 @@ public class Network {
 	 * @return The JSONObject containing the data, or an empty JSONObject if there was an error,
 	 * or the page timed out.
 	 */
+	@NonNull
 	public static JSONObject getJsonFromUrl(String url, boolean useTimeouts) {
+
 		// Log the url that was passed as an argument for debugging purposes
 		Log.i("getJsonFromUrl", "Url provided: " + url);
 
 		// Create a StringBuilder object that can be accessed within the thread.
 		// This will later be used to create the returned JSONObject.
-		final StringBuilder jsonString = new StringBuilder();
+		final StringBuilder jsonString = new StringBuilder(0);
 
 		// Run the following on a new thread (because android hates networking on the UI thread).
 		Thread t = new Thread(() -> {
 			try {
+
 				// Get the read result from the network thread.
 				String result = Network.readFromUrl(url, useTimeouts);
 				Log.v("getJsonFromUrl", "Returned string: " + result);
@@ -65,6 +70,7 @@ public class Network {
 				// Append the resulting string to the StringBuilder.
 				jsonString.append(result);
 			} catch (SocketTimeoutException e) {
+
 				// If the socket timed out while trying to get data from the url,
 				// simply return from the thread.
 				Log.w("getJsonFromUrl", "Connection timed out");
@@ -76,23 +82,30 @@ public class Network {
 		t.start();
 
 		try {
+
 			// Make sure the thread waits for the appropriate amount of time before continuing.
-			t.join(useTimeouts ? Network.CONNECTION_TIMEOUT + Network.READ_TIMEOUT + Network.PROCESSING_TIMEOUT : 0);
+			t.join(useTimeouts ? Network.CONNECTION_TIMEOUT + Network.READ_TIMEOUT +
+					Network.PROCESSING_TIMEOUT : 0);
 		} catch (InterruptedException e) {
-			// If this got interrupted at all, simply print the stacktrace.
-			e.printStackTrace();
+
+			// If this got interrupted at all then simply log the stacktrace.
+			Log.e("getJsonFromUrl", "Connection interrupted", e);
 		}
 
 		// Check if the string builder is empty.
 		if (jsonString.length() == 0) {
+
 			// Since its empty, return a new Json object.
 			return new JSONObject();
 		}
 
 		try {
-			// Try to create a new Json object using the string builder, and return the resulting Json object.
+
+			// Try to create a new Json object using the string builder,
+			// and return the resulting Json object.
 			return new JSONObject(jsonString.toString());
 		} catch (org.json.JSONException e) {
+
 			// Log if the Json object couldn't be created, and return an empty object.
 			Log.e("getJsonFromUrl", "Couldn't convert string to JSON", e);
 			return new JSONObject();
@@ -110,15 +123,23 @@ public class Network {
 	 * @return The string containing the Json string, which can then be parsed into a JSONObject.
 	 * @throws SocketTimeoutException Thrown if the specified timeout has been reached.
 	 */
+	@NonNull
+	@SuppressWarnings("RedundantThrows")
 	private static String readFromUrl(String url, boolean useTimeouts) throws SocketTimeoutException {
 
 		// Try connecting to the provided url.
 		java.net.URLConnection connection;
 		try {
 			connection = new java.net.URL(url).openConnection();
+		} catch (java.net.MalformedURLException e) {
+
+			// Log if the url was malformed, and return an empty string.
+			Log.e("readFromUrl", "Url is malformed!\n" + url, e);
+			return "";
 		} catch (IOException e) {
-			// If the connection was unsuccessful, log it and return an empty string.
-			Log.e("readFromUrl", "Could not connect to provided URL: " + url);
+
+			// If the connection was unsuccessful simply log it and return an empty string.
+			Log.e("readFromUrl", "Could not connect to provided URL: " + url, e);
 			return "";
 		}
 
@@ -131,24 +152,25 @@ public class Network {
 		try {
 			inputStream = connection.getInputStream();
 		} catch (IOException e) {
+
 			// If there was an exception thrown simply log it and return an empty string.
-			Log.e("readFromUrl", "Could not get an input stream from the connection");
+			Log.e("readFromUrl", "Could not get an input stream from the connection", e);
 			return "";
 		}
 
-		// Create a buffered reader for the input stream,
+		// Create a buffered reader for the input stream.
 		BufferedReader bufferedReader = new BufferedReader(new java.io.InputStreamReader(inputStream,
 				java.nio.charset.StandardCharsets.UTF_8));
 
-		// Store the inputted text into a string variable,
+		// Store the inputted text into a string variable.
 		String output = Network.readAll(bufferedReader);
-
 
 		// Try to close the buffered reader.
 		try {
 			bufferedReader.close();
 		} catch (IOException e) {
-			// If there was an exception thrown while closing the reader, log it.
+
+			// If there was an exception thrown while closing the reader simply log it.
 			Log.e("readFromUrl", "Could not close buffered reader", e);
 		}
 
@@ -156,6 +178,7 @@ public class Network {
 		try {
 			inputStream.close();
 		} catch (IOException e) {
+
 			// If there was an exception thrown while closing the stream, log it.
 			Log.e("readFromUrl", "Could not close input stream", e);
 		}
@@ -173,28 +196,36 @@ public class Network {
 	 * @param reader The Reader or BufferedReader object that will be used to read the character stream.
 	 * @return The final string from the String builder containing what was read by the Reader.
 	 */
-	private static String readAll(java.io.Reader reader) {
-		// Create a string to store what is read by the reader
-		StringBuilder string = new StringBuilder();
+	@NonNull
+	private static String readAll(@NonNull java.io.Reader reader) {
 
-		// Create a variable for character parsing
+		// Create a string to store what is read by the reader.
+		StringBuilder string = new StringBuilder(0);
+
+		// Create a variable for character parsing.
 		int character;
 
 		// Try continuously reading characters from the reader until there is either an exception,
 		// or -1 is returned.
 		try {
+
 			// Make sure there are still characters to read from the reader.
-			while ((character = reader.read()) != -1) {
+			character = reader.read();
+			while (character != -1) {
+
 				// Append the character to the string
 				string.append((char) character);
+
+				// Read the next character and loop.
+				character = reader.read();
 			}
 		} catch (IOException e) {
+
 			// Log if an exception was thrown.
 			Log.e("readAll", "An exception was thrown while reading from the reader", e);
 		}
 
-		// Finally return the string
+		// Finally return the string.
 		return string.toString();
 	}
-
 }
