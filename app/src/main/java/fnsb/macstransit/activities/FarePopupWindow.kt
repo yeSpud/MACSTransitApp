662209804ce35561fa1e0c467b7fa3a41438b909
Spud <jeffrypig23@gmail.com>
2021-08-21 17:40:15 -0700

    Moved FarePopupWindow to binder

--- app/src/main/java/fnsb/macstransit/activities/FarePopupWindow.kt
+++ app/src/main/java/fnsb/macstransit/activities/FarePopupWindow.kt
@@ -1,8 +1,7 @@
 package fnsb.macstransit.activities
 
 import android.content.Intent
-import android.view.View
-import fnsb.macstransit.R
+import fnsb.macstransit.databinding.FaresPopupBinding
 
 /**
  * Created by Spud on 2020-02-03 for the project: MACS Transit.
@@ -19,23 +18,22 @@ class FarePopupWindow(context: android.content.Context) : android.app.AlertDialo
 	 */
 	fun showFarePopupWindow() {
 
-		// Find and inflate the settings view.
-		val dialogView = android.view.LayoutInflater.from(this.context)
-				.inflate(R.layout.fares_popup, findViewById(R.id.content), false)
+		// Comments
+		val binder: FaresPopupBinding = FaresPopupBinding.inflate(this.layoutInflater)
 
 		// Setup the hyperlink.
-		dialogView.findViewById<View>(R.id.link).setOnClickListener {
+		binder.link.setOnClickListener {
 			this.context.startActivity(Intent(Intent.ACTION_VIEW, android.net.Uri.parse(
 					"https://www.fnsb.gov/352/Bus-Fares")))
 		}
 
 		// Create the dialog via the alert dialog builder.
 		val builder = Builder(this.context)
-		builder.setView(dialogView)
+		builder.setView(binder.root)
 		val alertDialog = builder.create()
 
 		// Setup the cancel button.
-		dialogView.findViewById<View>(R.id.close).setOnClickListener { alertDialog.cancel() }
+		binder.close.setOnClickListener { alertDialog.cancel() }
 
 		// Show the dialog.
 		alertDialog.show()
