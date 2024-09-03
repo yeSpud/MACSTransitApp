4be1499dfff833fc5a41db37e29d489ab4b41582
Spud <10385195+yeSpud@users.noreply.github.com>
2024-09-03 14:31:42 -0800

    Updated stop entry proof of concept

--- app/src/main/java/fnsb/macstransit/activities/mapsactivity/mappopups/StopEntry.kt
+++ app/src/main/java/fnsb/macstransit/activities/mapsactivity/mappopups/StopEntry.kt
@@ -8,19 +8,15 @@ import fnsb.macstransit.R
 
 class StopEntry : LinearLayout {
 
-	val routeName: TextView
-
-	val stopTime: TextView
+	val routeName: TextView?
+	val stopTime: TextView?
 
 	constructor(context: Context): this(context, null)
-
 	constructor(context: Context, attributeSet: AttributeSet?): this(context, attributeSet, 0)
-
 	constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : super(context,
 	                                                                                      attributeSet,
 	                                                                                      defStyleAttr) {
 		inflate(context, R.layout.stop_entry, this)
-
 		routeName = findViewById(R.id.route_name)
 		stopTime = findViewById(R.id.stop_name)
 	}
